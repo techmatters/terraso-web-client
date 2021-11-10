7caf1c98eb276293cdf4966c481938467200663c
Jose Buitron <jose.buitron@gmail.com>
2021-11-10 17:03:22 -0600

    Material UI added (#2)

--- src/index.js
+++ src/index.js
@@ -5,18 +5,63 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  IconButton
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#5e5547',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Box>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
