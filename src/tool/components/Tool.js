2b6d75262aef65018d630ecf21cb269cf851ed94
Paul Schreiber <paul@technmatters.org>
2021-12-16 09:27:07 -0500

    feat: add tool component

---
+++ src/tool/components/Tool.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  Card,
+  Link,
+  Stack,
+  Typography
+} from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import theme from 'theme'
+
+const Tool = ({ tool }) => {
+  const { t } = useTranslation()
+
+  return (
+    <React.Fragment>
+      <Card sx={{ padding: theme.spacing(2) }}>
+        <Typography variant="h1" >
+          {tool.title}
+        </Typography>
+
+        <Stack direction="row" justifyContent="space-between" spacing={2}>
+
+          <section>
+            <Typography variant="h2" >
+              {t('tool.is_for')}
+            </Typography>
+
+            <ul>
+              {
+                tool.description.map((description, index) => (
+                  <li key={index}>{description}</li>
+                ))
+              }
+            </ul>
+
+            <Typography variant="h2" >
+              {t('tool.requirements')}
+            </Typography>
+
+            <ul>
+              {
+                tool.requirements.map((req, index) => (
+                  <li key={index}>{req}</li>
+                ))
+              }
+            </ul>
+          </section>
+
+          <section>
+            <Link href={tool.url}>
+              <img alt={tool.title} height={tool.img.height} width={tool.img.src} src={tool.img.src} />
+            </Link>
+
+            <p>
+              <Link href={tool.url}>
+                {t('tool.go_to', { tool: tool.title })}
+              </Link>
+            </p>
+          </section>
+        </Stack>
+      </Card>
+
+    </React.Fragment>
+  )
+}
+
+export default Tool
