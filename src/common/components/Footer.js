16757f7cbba6a9f2ad6aec63cc97a62591ce8116
Paul Schreiber <paul@technmatters.org>
2021-12-16 12:39:19 -0500

    refactor: move footer links in to array

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -12,15 +12,21 @@ const year = new Date().getFullYear();
 const Footer = () => {
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const footerLinks = [
+    { url: '#', text: 'Terraso Help' },
+    { url: 'https://terraso.org/contact-us/', text: 'Contact' },
+    { url: '#', text: 'Terms of Use' },
+    { url: 'https://techmatters.org/privacy-policy/', text: 'Privacy Policy' },
+    { url: '#', text: 'Data Policy' }
+  ];
+
   return (
     <footer>
       <Stack direction={isSmall ? 'column' : 'row'} justifyContent="space-between" spacing={2} sx={{ color: theme.palette.white, background: theme.palette.secondary.main }}>
         <ul style={{ listStyle: 'none' }}>
-          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terraso Help</Link></li>
-          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://terraso.org/contact-us/">Contact</Link></li>
-          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terms of Use</Link></li>
-          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://techmatters.org/privacy-policy/">Privacy Policy</Link></li>
-          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Data Policy</Link></li>
+          {
+            footerLinks.map(link => <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="{link.url}">{link.text}</Link></li>)
+          }
         </ul>
 
         <ul style={{ listStyle: 'none', display: 'flex', alignItems: 'center', paddingRight: '40px' }}>
