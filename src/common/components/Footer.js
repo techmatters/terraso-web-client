f0fbe50dbbbe032da6debbdb2dc6f9f1f4f6fa8e
Paul Schreiber <paul@technmatters.org>
2021-12-16 11:41:43 -0500

    fix: make footer responsive

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import {
   Link,
   Stack
@@ -8,20 +9,26 @@ import theme from 'theme';
 
 const year = new Date().getFullYear();
 
-const Footer = () => (
-  <footer>
-    <Stack direction="row" justifyContent="space-between" spacing={2} sx={{ color: theme.palette.white, background: theme.palette.secondary.main }}>
-      <ul style={{ listStyle: 'none' }}>
-        <li style={{ display: 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terraso Help</Link></li>
-        <li style={{ display: 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://terraso.org/contact-us/">Contact</Link></li>
-        <li style={{ display: 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terms of Use</Link></li>
-        <li style={{ display: 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://techmatters.org/privacy-policy/">Privacy Policy</Link></li>
-        <li style={{ display: 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Data Policy</Link></li>
-      </ul>
+const Footer = () => {
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
-      <p style={{ display: 'flex', alignItems: 'center', paddingRight: '40px' }}>© {year} Tech Matters</p>
-    </Stack>
-  </footer>
-);
+  return (
+    <footer>
+      <Stack direction={isSmall ? 'column' : 'row'} justifyContent="space-between" spacing={2} sx={{ color: theme.palette.white, background: theme.palette.secondary.main }}>
+        <ul style={{ listStyle: 'none' }}>
+          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terraso Help</Link></li>
+          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://terraso.org/contact-us/">Contact</Link></li>
+          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Terms of Use</Link></li>
+          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="https://techmatters.org/privacy-policy/">Privacy Policy</Link></li>
+          <li style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="#">Data Policy</Link></li>
+        </ul>
+
+        <ul style={{ listStyle: 'none', display: 'flex', alignItems: 'center', paddingRight: '40px' }}>
+          <li>© {year} Tech Matters</li>
+        </ul>
+      </Stack>
+    </footer>
+  );
+};
 
 export default Footer;
