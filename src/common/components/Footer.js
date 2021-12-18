0e965bad36ee8218026317f0b83db06b9d6662ab
Jose Buitron <jose@techmatters.org>
2021-12-17 19:01:02 -0500

    fix: Footer update

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -1,38 +1,114 @@
 import React from 'react'
-import useMediaQuery from '@mui/material/useMediaQuery'
+// import useMediaQuery from '@mui/material/useMediaQuery'
 import {
   Link,
-  Stack
+  Divider,
+  Typography,
+  Container,
+  Grid
 } from '@mui/material'
 
 import theme from 'theme'
 
 const year = new Date().getFullYear()
 
-const Footer = () => {
-  const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
+const footerLinks = [
+  { url: '#', text: 'Terraso Help' },
+  { url: 'https://terraso.org/contact-us/', text: 'Contact' },
+  { url: '#', text: 'Terms of Use' },
+  { url: 'https://techmatters.org/privacy-policy/', text: 'Privacy Policy' },
+  { url: '#', text: 'Data Policy' }
+]
 
-  const footerLinks = [
-    { url: '#', text: 'Terraso Help' },
-    { url: 'https://terraso.org/contact-us/', text: 'Contact' },
-    { url: '#', text: 'Terms of Use' },
-    { url: 'https://techmatters.org/privacy-policy/', text: 'Privacy Policy' },
-    { url: '#', text: 'Data Policy' }
-  ]
-
+const FooterLink = ({ index, link }) => {
   return (
-    <footer style={{ position: 'fixed', bottom: 0, width: '100%' }}>
-      <Stack direction={isSmall ? 'column' : 'row'} justifyContent="space-between" spacing={2} sx={{ color: theme.palette.white, background: theme.palette.secondary.main }}>
-        <ul style={{ listStyle: 'none' }}>
-          {
-            footerLinks.map((link, index) => <li key={index} style={{ display: isSmall ? 'block' : 'inline', padding: '0 5px' }}><Link sx={{ color: theme.palette.white }} href="{link.url}">{link.text}</Link></li>)
+    <React.Fragment>
+      <Grid item component="li"
+        xs={12} sm="auto"
+        sx={{
+          paddingBottom: {
+            xs: theme.spacing(1),
+            sm: 0
           }
-        </ul>
+        }}
+      >
+        <Link
+          variant="body2"
+          underline="none"
+          href={link.url}
+          sx={{ color: theme.palette.white }}
+        >
+          {link.text}
+        </Link>
+      </Grid>
+      {index < footerLinks.length - 1 && (
+        <Divider
+          flexItem
+          orientation="vertical"
+          sx={{
+            bgcolor: 'white',
+            marginLeft: theme.spacing(2),
+            marginRight: theme.spacing(2)
+          }}
+        />
+      )}
+    </React.Fragment>
+  )
+}
 
-        <ul style={{ listStyle: 'none', display: 'flex', alignItems: 'center', paddingRight: '40px' }}>
-          <li>© {year} Tech Matters</li>
-        </ul>
-      </Stack>
+const Footer = () => {
+  return (
+    <footer style={{
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+      background: theme.palette.secondary.main
+    }}>
+      <Container>
+        <Grid container
+          justifyContent="space-between"
+          sx={{
+            color: theme.palette.white,
+            padding: theme.spacing(2)
+          }}
+        >
+          <Grid item xs={12} md={8}>
+            <Grid container
+              component="ul"
+              spacing={0}
+              sx= {{
+                listStyle: 'none',
+                padding: 0,
+                margin: 0
+              }}
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              {footerLinks.map((link, index) => (
+                <FooterLink key={index} index={index} link={link} />
+              ))}
+            </Grid>
+          </Grid>
+          <Grid item
+            xs={12}
+            sm="auto"
+          >
+            <Typography
+              variant="body2"
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                marginTop: {
+                  xs: theme.spacing(4),
+                  md: 0
+                }
+              }}
+            >
+              © {year} Tech Matters
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </footer>
   )
 }
