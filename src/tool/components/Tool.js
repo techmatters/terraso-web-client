7739b841c4fefeb285043a49f79f8245d62b8784
Paul Schreiber <paul@technmatters.org>
2021-12-20 11:41:57 -0500

    fix: add icon beside tool links

--- src/tool/components/Tool.js
+++ src/tool/components/Tool.js
@@ -5,6 +5,7 @@ import {
   Stack,
   Typography
 } from '@mui/material'
+import LaunchIcon from '@mui/icons-material/Launch'
 import { useTranslation } from 'react-i18next'
 import theme from 'theme'
 
@@ -54,6 +55,7 @@ const Tool = ({ tool }) => {
             <p>
               <Link href={tool.url}>
                 {t('tool.go_to', { tool: tool.title })}
+                <LaunchIcon sx={{ paddingLeft: '5px', height: '1.2rem', width: '1.2rem', verticalAlign: 'bottom' }} />
               </Link>
             </p>
           </section>
