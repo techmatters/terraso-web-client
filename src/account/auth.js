7cb4370012bd4ed336bb188df9b7f84489c64b01
Jose Buitron <jose@techmatters.org>
2021-12-23 18:35:05 -0500

    fix: Set cookie domain for delete

--- src/account/auth.js
+++ src/account/auth.js
@@ -2,4 +2,8 @@ import Cookies from 'js-cookie'
 
 export const getToken = () => Cookies.get('atoken')
 
-export const removeToken = () => Cookies.remove('atoken', { path: '' })
+export const removeToken = () => {
+  const domain = window.location.hostname
+  console.log({ domain })
+  Cookies.remove('atoken', { path: '/', domain })
+}
