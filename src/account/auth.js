e54e24a640bb333dff589b56f262a0775e05f1fe
Jose Buitron <jose@techmatters.org>
2021-12-23 18:39:06 -0500

    fix: Cookie domain

--- src/account/auth.js
+++ src/account/auth.js
@@ -3,7 +3,5 @@ import Cookies from 'js-cookie'
 export const getToken = () => Cookies.get('atoken')
 
 export const removeToken = () => {
-  const domain = window.location.hostname
-  console.log({ domain })
-  Cookies.remove('atoken', { path: '/', domain })
+  Cookies.remove('atoken', { path: '/', domain: '.terraso.org' })
 }
