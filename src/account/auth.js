43460d5f0ff560f48fdc4429dd2b670c20cbda23
Jose Buitron <jose@techmatters.org>
2021-12-23 18:42:25 -0500

    fix: Remove refresh token too

--- src/account/auth.js
+++ src/account/auth.js
@@ -1,7 +1,10 @@
 import Cookies from 'js-cookie'
 
+const COOKIES_DOMAIN = '.terraso.org'
+
 export const getToken = () => Cookies.get('atoken')
 
 export const removeToken = () => {
-  Cookies.remove('atoken', { path: '/', domain: '.terraso.org' })
+  Cookies.remove('rtoken', { path: '/', domain: COOKIES_DOMAIN })
+  Cookies.remove('atoken', { path: '/', domain: COOKIES_DOMAIN })
 }
