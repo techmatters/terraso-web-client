8dba83bd2959029d87ae901cb0e53ba8c102cdc9
Jose Buitron <jose@techmatters.org>
2021-12-23 18:22:15 -0500

    fix: Delete token cookie

--- src/account/auth.js
+++ src/account/auth.js
@@ -2,4 +2,4 @@ import Cookies from 'js-cookie'
 
 export const getToken = () => Cookies.get('atoken')
 
-export const removeToken = () => Cookies.remove('atoken')
+export const removeToken = () => Cookies.remove('atoken', { path: '' })
