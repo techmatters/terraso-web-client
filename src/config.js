c804fe2db549ba196e18b92bb066ccd9bb70b4bf
Paul Schreiber <paul@technmatters.org>
2022-01-05 17:44:37 -0500

    fix: use 127.0.0.1 instead of localhost for default URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const TERRASO_API_URL = process.env.REACT_APP_TERRASO_API_URL || 'http://localhost:8000'
+export const TERRASO_API_URL = process.env.REACT_APP_TERRASO_API_URL || 'http://127.0.0.1:8000'
 
 export const GRAPH_QL_ENDPOINT = 'graphql/'
 
