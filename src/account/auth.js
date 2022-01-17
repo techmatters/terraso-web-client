17507930e8c84a6ed7e019d4a3ce772ba96dadec
Jose Buitron <jose.buitron@gmail.com>
2022-01-17 17:13:49 -0500

    feat: Renew tokens (#163)

--- src/account/auth.js
+++ src/account/auth.js
@@ -1,13 +1,38 @@
 import Cookies from 'js-cookie'
 import jwt from 'jwt-decode'
 
-import { COOKIES_DOMAIN } from 'config'
+import { COOKIES_DOMAIN, TERRASO_API_URL } from 'config'
+import { UNAUTHENTICATED } from 'account/authConstants'
+
+const COOKIES_PARAMS = { path: '/', domain: COOKIES_DOMAIN }
 
 export const getToken = () => Cookies.get('atoken')
 
 export const removeToken = () => {
-  Cookies.remove('rtoken', { path: '/', domain: COOKIES_DOMAIN })
-  Cookies.remove('atoken', { path: '/', domain: COOKIES_DOMAIN })
+  Cookies.remove('rtoken', COOKIES_PARAMS)
+  Cookies.remove('atoken', COOKIES_PARAMS)
+}
+
+export const refreshToken = async () => {
+  const response = await fetch(
+    new URL('/auth/tokens', TERRASO_API_URL).href,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ refresh_token: Cookies.get('rtoken') })
+    }
+  )
+
+  if (response.status !== 200) {
+    await Promise.reject(UNAUTHENTICATED)
+  }
+
+  const tokens = await response.json()
+
+  const { access_token: atoken, refresh_token: rtoken } = tokens
+
+  Cookies.set('rtoken', rtoken, COOKIES_PARAMS)
+  Cookies.set('atoken', atoken, COOKIES_PARAMS)
 }
 
 export const getUserEmail = () => jwt(Cookies.get('atoken')).email
--- src/account/components/RequireAuth.test.js
+++ src/account/components/RequireAuth.test.js
@@ -10,7 +10,10 @@ import { getUserEmail } from 'account/auth'
 
 jest.mock('terrasoBackend/api')
 
-jest.mock('account/auth')
+jest.mock('account/auth', () => ({
+  ...jest.requireActual('account/auth'),
+  getUserEmail: jest.fn()
+}))
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -20,11 +23,18 @@ jest.mock('react-router-dom', () => ({
   )
 }))
 
+beforeEach(() => {
+  global.fetch = jest.fn()
+})
+
 test('Auth: test redirect', async () => {
   useParams.mockReturnValue({
     slug: 'slug-1'
   })
-  terrasoApi.request.mockRejectedValue('UNAUTHENTICATED')
+  terrasoApi.request.mockRejectedValueOnce('UNAUTHENTICATED')
+  global.fetch.mockResolvedValueOnce({
+    status: 401
+  })
   await act(async () => render(
     <RequireAuth><GroupView /></RequireAuth>,
     {
@@ -41,7 +51,41 @@ test('Auth: test redirect', async () => {
       }
     }
   ))
+  expect(global.fetch).toHaveBeenCalledTimes(1)
+  expect(terrasoApi.request).toHaveBeenCalledTimes(1)
   expect(screen.getByText('To: /account')).toBeInTheDocument()
+})
+test('Auth: test refresh tokens', async () => {
+  useParams.mockReturnValue({
+    slug: 'slug-1'
+  })
+  terrasoApi.request
+    .mockRejectedValueOnce('UNAUTHENTICATED')
+    .mockResolvedValueOnce({})
+  global.fetch.mockResolvedValueOnce({
+    status: 200,
+    json: () => ({
+      atoken: 'auth-token',
+      rtoken: 'refresh-token'
+    })
+  })
+  await act(async () => render(
+    <RequireAuth><GroupView /></RequireAuth>,
+    {
+      account: {
+        hasToken: true,
+        currentUser: {
+          fetching: false,
+          data: {
+            email: 'email@email.com',
+            firstName: 'John',
+            lastName: 'Doe'
+          }
+        }
+      }
+    }
+  ))
+  expect(screen.getByText('Group not found')).toBeInTheDocument()
 })
 test('Auth: test fetch user', async () => {
   getUserEmail.mockReturnValue(Promise.resolve('test@email.com'))
