6be4b72c627f77b0d81be254830465a1258a9ced
Caio Carrara <caio@techmatters.org>
2022-02-11 14:45:53 -0300

    feat: allow Landscape map zooming with mouse wheel

--- src/gis/components/Map.js
+++ src/gis/components/Map.js
@@ -5,7 +5,7 @@ import 'gis/components/Map.css';
 
 const Map = props => {
   return (
-    <MapContainer scrollWheelZoom={false} {...props}>
+    <MapContainer zoomDelta={0.5} wheelPxPerZoomLevel={100} {...props}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
