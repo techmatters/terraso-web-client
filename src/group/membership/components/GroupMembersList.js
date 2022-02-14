b9e3898100d19a0cc4a5feb4a517f4407fca207f
Jose Buitron <jose.buitron@gmail.com>
2022-02-14 16:15:22 -0500

    feat: Responsive Table

--- src/group/membership/components/GroupMembersList.js
+++ src/group/membership/components/GroupMembersList.js
@@ -3,24 +3,14 @@ import _ from 'lodash/fp';
 import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import {
-  Card,
-  Grid,
-  List,
-  ListItem,
-  MenuItem,
-  Select,
-  Stack,
-  Typography,
-} from '@mui/material';
+import { MenuItem, Select, Stack, Typography } from '@mui/material';
 
 import { fetchMembers, removeMember, updateMemberRole } from 'group/groupSlice';
 import { useGroupContext } from 'group/groupContext';
-import Table from 'common/components/Table';
 import AccountAvatar from 'account/components/AccountAvatar';
 import Restricted from 'permissions/components/Restricted';
 import PageLoader from 'layout/PageLoader';
+import ResponsiveTable from 'common/components/ResponsiveTable';
 import theme from 'theme';
 
 const ROLES = ['MEMBER', 'MANAGER'];
@@ -93,120 +83,10 @@ const RemoveButton = ({ member }) => {
   );
 };
 
-const MembersTable = () => {
-  const { t } = useTranslation();
-  const { members } = useContext(GroupMembersListContext);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const columns = [
-    {
-      field: 'name',
-      headerName: t('group.members_list_column_name'),
-      flex: 1.5,
-      minWidth: 200,
-      valueGetter: ({ row: member }) => t('user.full_name', { user: member }),
-      renderCell: ({ row: member }) => (
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={2}
-        >
-          <AccountAvatar sx={{ width: 34, height: 34 }} user={member} />
-          <Typography>{t('user.full_name', { user: member })}</Typography>
-        </Stack>
-      ),
-    },
-    {
-      field: 'role',
-      type: 'actions',
-      headerName: t('group.members_list_column_role'),
-      flex: 1.5,
-      minWidth: 200,
-      valueGetter: ({ row: member }) =>
-        t(`group.role_${member.role.toLowerCase()}`),
-      getActions: ({ row: member }) => [<RoleSelect member={member} />],
-    },
-    {
-      field: 'actions',
-      headerName: t('group.members_list_column_actions_description'),
-      type: 'actions',
-      sortable: false,
-      flex: 1.5,
-      minWidth: 200,
-      align: 'center',
-      getActions: ({ row: member }) => [<RemoveButton member={member} />],
-    },
-  ];
-
-  return (
-    <Table
-      rows={members}
-      columns={columns}
-      initialSort={[
-        {
-          field: 'name',
-          sort: 'asc',
-        },
-      ]}
-      searchParams={Object.fromEntries(searchParams.entries())}
-      onSearchParamsChange={setSearchParams}
-      localeText={{
-        noRowsLabel: t('group.members_list_empty'),
-      }}
-    />
-  );
-};
-
-const MembersCards = () => {
-  const { t } = useTranslation();
-  const { members } = useContext(GroupMembersListContext);
-
-  return (
-    <List>
-      {members.map(member => (
-        <ListItem
-          key={member.email}
-          sx={{ padding: 0, marginBottom: theme.spacing(2) }}
-        >
-          <Card
-            component={Stack}
-            direction="row"
-            alignItems="center"
-            spacing={1}
-            sx={{ width: '100%', padding: theme.spacing(1) }}
-          >
-            <AccountAvatar sx={{ width: 80, height: 80 }} user={member} />
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="caption">
-                  {t('group.members_list_column_name')}
-                </Typography>
-                <Typography variant="body1">
-                  {t('user.full_name', { user: member })}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="caption" display="block">
-                  {t('group.members_list_column_role')}
-                </Typography>
-                <RoleSelect member={member} />
-              </Grid>
-              <Grid item xs={6}>
-                <RemoveButton member={member} />
-              </Grid>
-            </Grid>
-          </Card>
-        </ListItem>
-      ))}
-    </List>
-  );
-};
-
 const GroupMembersList = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
-  const isSmall = useMediaQuery(theme.breakpoints.down('md'));
+  const [searchParams, setSearchParams] = useSearchParams();
   const { list: members, fetching } = useSelector(state => state.group.members);
 
   const { owner, groupSlug } = useGroupContext();
@@ -233,6 +113,52 @@ const GroupMembersList = () => {
     members: _.values(members),
   };
 
+  const columns = [
+    {
+      field: 'name',
+      headerName: t('group.members_list_column_name'),
+      flex: 1.5,
+      minWidth: 200,
+      valueGetter: ({ row: member }) => t('user.full_name', { user: member }),
+      cardRender: ({ row: member }) => (
+        <Typography>{t('user.full_name', { user: member })}</Typography>
+      ),
+      renderCell: ({ row: member }) => (
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={2}
+        >
+          <AccountAvatar sx={{ width: 34, height: 34 }} user={member} />
+          <Typography>{t('user.full_name', { user: member })}</Typography>
+        </Stack>
+      ),
+    },
+    {
+      field: 'role',
+      type: 'actions',
+      headerName: t('group.members_list_column_role'),
+      flex: 1.5,
+      minWidth: 200,
+      cardSize: 6,
+      valueGetter: ({ row: member }) =>
+        t(`group.role_${member.role.toLowerCase()}`),
+      getActions: ({ row: member }) => [<RoleSelect member={member} />],
+    },
+    {
+      field: 'actions',
+      headerName: t('group.members_list_column_actions_description'),
+      type: 'actions',
+      sortable: false,
+      flex: 1.5,
+      minWidth: 200,
+      align: 'center',
+      cardSize: 6,
+      getActions: ({ row: member }) => [<RemoveButton member={member} />],
+    },
+  ];
+
   return (
     <GroupMembersListContext.Provider value={listContext}>
       <Stack
@@ -250,7 +176,28 @@ const GroupMembersList = () => {
           </Typography>
         </Restricted>
       </Stack>
-      {isSmall ? <MembersCards /> : <MembersTable />}
+      <ResponsiveTable
+        columns={columns}
+        rows={_.values(members)}
+        cardsProps={{
+          avatarRender: ({ row: member }) => (
+            <AccountAvatar sx={{ width: 80, height: 80 }} user={member} />
+          ),
+        }}
+        tableProps={{
+          initialSort: [
+            {
+              field: 'name',
+              sort: 'asc',
+            },
+          ],
+          searchParams: Object.fromEntries(searchParams.entries()),
+          onSearchParamsChange: setSearchParams,
+          localeText: {
+            noRowsLabel: t('group.members_list_empty'),
+          },
+        }}
+      />
     </GroupMembersListContext.Provider>
   );
 };
--- src/landscape/components/LandscapeList.js
+++ src/landscape/components/LandscapeList.js
@@ -2,30 +2,21 @@ import React, { useEffect } from 'react';
 import _ from 'lodash/fp';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import { Link as RouterLink, useSearchParams } from 'react-router-dom';
-import {
-  Box,
-  Typography,
-  Link,
-  Grid,
-  Card,
-  List,
-  ListItem,
-} from '@mui/material';
+import { Typography, Link } from '@mui/material';
 
 import { fetchLandscapes } from 'landscape/landscapeSlice';
 import { withProps } from 'react-hoc';
 import { useDocumentTitle } from 'common/document';
 import GroupMembershipButton from 'group/membership/components/GroupMembershipButton';
 import GroupMembershipCount from 'group/membership/components/GroupMembershipCount';
-import Table from 'common/components/Table';
 import PageLoader from 'layout/PageLoader';
 import PageHeader from 'layout/PageHeader';
 import PageContainer from 'layout/PageContainer';
 import { GroupContextProvider } from 'group/groupContext';
 import LandscapeMemberLeave from 'landscape/membership/components/LandscapeMemberLeave';
 import GroupMemberJoin from 'group/membership/components/GroupMemberJoin';
+import ResponsiveTable from 'common/components/ResponsiveTable';
 
 import theme from 'theme';
 
@@ -56,9 +47,21 @@ const MembershipButton = ({ landscape }) => (
   </GroupContextProvider>
 );
 
-const LandscapeTable = ({ landscapes }) => {
+const LandscapeList = () => {
+  const dispatch = useDispatch();
   const { t } = useTranslation();
   const [searchParams, setSearchParams] = useSearchParams();
+  const { landscapes, fetching } = useSelector(state => state.landscape.list);
+
+  useDocumentTitle(t('landscape.list_document_title'));
+
+  useEffect(() => {
+    dispatch(fetchLandscapes());
+  }, [dispatch]);
+
+  if (fetching) {
+    return <PageLoader />;
+  }
 
   const columns = [
     {
@@ -95,8 +98,9 @@ const LandscapeTable = ({ landscapes }) => {
       field: 'members',
       headerName: t('landscape.list_column_members'),
       align: 'center',
+      cardSize: 6,
       valueGetter: ({ row: landscape }) =>
-        _.getOr(0, 'defaultGroup.members.length', landscape),
+        _.getOr(0, 'defaultGroup.membersInfo.totalCount', landscape),
       renderCell: ({ row: landscape }) => (
         <GroupMembershipCount groupSlug={landscape.defaultGroup.slug} />
       ),
@@ -107,111 +111,12 @@ const LandscapeTable = ({ landscapes }) => {
       headerName: t('landscape.list_column_actions_description'),
       sortable: false,
       align: 'center',
+      cardSize: 6,
       getActions: ({ row: landscape }) => [
         <MembershipButton landscape={landscape} />,
       ],
     },
   ];
-
-  return (
-    <Table
-      rows={landscapes}
-      columns={columns}
-      initialSort={[
-        {
-          field: 'name',
-          sort: 'asc',
-        },
-      ]}
-      searchParams={Object.fromEntries(searchParams.entries())}
-      onSearchParamsChange={setSearchParams}
-      localeText={{
-        noRowsLabel: t('landscape.list_empty'),
-        footerPaginationRowsPerPage: t('common.data_grid_pagination_of'),
-      }}
-    />
-  );
-};
-
-const LandscapeCards = ({ landscapes }) => {
-  const { t } = useTranslation();
-
-  return (
-    <List>
-      {landscapes.map(landscape => (
-        <ListItem
-          key={landscape.slug}
-          sx={{ padding: 0, marginBottom: theme.spacing(2) }}
-        >
-          <Card sx={{ padding: theme.spacing(2), width: '100%' }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="caption">
-                  {t('landscape.list_column_name')}
-                </Typography>
-                <Link
-                  variant="body1"
-                  display="block"
-                  component={RouterLink}
-                  to={`/landscapes/${landscape.slug}`}
-                >
-                  {landscape.name}
-                </Link>
-              </Grid>
-              {landscape.location && (
-                <Grid item xs={12}>
-                  <Typography variant="caption">
-                    {t('landscape.list_column_location')}
-                  </Typography>
-                  <Typography variant="body1">{landscape.location}</Typography>
-                </Grid>
-              )}
-              {landscape.website && (
-                <Grid item xs={12}>
-                  <Typography variant="caption">
-                    {t('landscape.list_column_website')}
-                  </Typography>
-                  <Link
-                    component={Box}
-                    href={landscape.website}
-                    underline="none"
-                  >
-                    {landscape.website}
-                  </Link>
-                </Grid>
-              )}
-              <Grid item xs={6}>
-                <Typography variant="caption">
-                  {t('landscape.list_column_members')}
-                </Typography>
-                <GroupMembershipCount groupSlug={landscape.defaultGroup.slug} />
-              </Grid>
-              <Grid item xs={6}>
-                <MembershipButton landscape={landscape} />
-              </Grid>
-            </Grid>
-          </Card>
-        </ListItem>
-      ))}
-    </List>
-  );
-};
-
-const LandscapeList = () => {
-  const dispatch = useDispatch();
-  const { t } = useTranslation();
-  const { landscapes, fetching } = useSelector(state => state.landscape.list);
-  const isSmall = useMediaQuery(theme.breakpoints.down('md'));
-
-  useDocumentTitle(t('landscape.list_document_title'));
-
-  useEffect(() => {
-    dispatch(fetchLandscapes());
-  }, [dispatch]);
-
-  if (fetching) {
-    return <PageLoader />;
-  }
 
   return (
     <PageContainer>
@@ -224,11 +129,24 @@ const LandscapeList = () => {
           marginTop: theme.spacing(2),
         }}
       ></Typography>
-      {isSmall ? (
-        <LandscapeCards landscapes={landscapes} />
-      ) : (
-        <LandscapeTable landscapes={landscapes} />
-      )}
+      <ResponsiveTable
+        columns={columns}
+        rows={landscapes}
+        tableProps={{
+          initialSort: [
+            {
+              field: 'name',
+              sort: 'asc',
+            },
+          ],
+          searchParams: Object.fromEntries(searchParams.entries()),
+          onSearchParamsChange: setSearchParams,
+          localeText: {
+            noRowsLabel: t('landscape.list_empty'),
+            footerPaginationRowsPerPage: t('common.data_grid_pagination_of'),
+          },
+        }}
+      />
     </PageContainer>
   );
 };
