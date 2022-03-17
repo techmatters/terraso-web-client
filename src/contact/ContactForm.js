720fef31b2ecceef8ec4aa307f2e93614a2f1125
Jose Buitron <jose.buitron@gmail.com>
2022-03-17 17:25:21 -0500

    fix: A11y improvement

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -12,12 +12,14 @@ import { HUBSPOT_FORMS } from 'config';
 
 const ContactForm = () => {
   const dispatch = useDispatch();
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const { data: user } = useSelector(state => state.account.currentUser);
   const [loadingForm, setLoadingForm] = useState(true);
 
   const loadingDependencies = useScript('//js.hsforms.net/forms/shell.js');
   const loading = loadingDependencies || loadingForm;
+
+  const iframeTitle = t('contact.iframe_title');
 
   useEffect(() => {
     if (loadingDependencies) {
@@ -40,12 +42,22 @@ const ContactForm = () => {
       }
       // Available events: https://legacydocs.hubspot.com/global-form-events
       switch (_.get('data.eventName', event)) {
-        case 'onFormReady':
+        case 'onFormReady': {
+          // Set user email
           if (form.hasField('email')) {
             form.setFieldValue('email', user.email);
           }
+          // Set iframe title
+          const iframe = _.head(
+            document.getElementsByClassName('hs-form-iframe')
+          );
+          if (iframe) {
+            iframe.setAttribute('title', iframeTitle);
+          }
+
           setLoadingForm(false);
           break;
+        }
         case 'onFormSubmit':
           dispatch(
             addMessage({
@@ -63,7 +75,7 @@ const ContactForm = () => {
     return () => {
       window.removeEventListener('message', onMessage);
     };
-  }, [loadingDependencies, dispatch, user.email, i18n.language]);
+  }, [loadingDependencies, dispatch, user.email, i18n.language, iframeTitle]);
 
   return (
     <>
