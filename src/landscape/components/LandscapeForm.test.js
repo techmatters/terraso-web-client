d064260d57cc668be584e81649210e464f94b4d4
Paul Schreiber <paul@techmatters.org>
2022-04-18 11:41:29 -0400

    fix: Improve labels and titles for group and landscape editing (#306)
    * fix: distinguish between Save and Create button labels
    * fix: update strings for groups labels and titles
    
    Co-authored-by: Jose Buitron <jose.buitron@gmail.com>

--- src/landscape/components/LandscapeForm.test.js
+++ src/landscape/components/LandscapeForm.test.js
@@ -138,9 +138,7 @@ test('LandscapeForm: Input validation', async () => {
   fireEvent.change(inputs.website, { target: { value: 'wwwotherorg' } });
   expect(inputs.website).toHaveValue('wwwotherorg');
 
-  await act(async () =>
-    fireEvent.click(screen.getByText(/Submit Landscape Info/i))
-  );
+  await act(async () => fireEvent.click(screen.getByText(/Save Changes/i)));
   expect(screen.getByText(/name is a required field/i)).toBeInTheDocument();
   expect(
     screen.getByText(/description is a required field/i)
@@ -188,9 +186,7 @@ test('LandscapeForm: Save form', async () => {
   });
   fireEvent.change(inputs.location, { target: { value: 'New location' } });
 
-  await act(async () =>
-    fireEvent.click(screen.getByText(/Submit Landscape Info/i))
-  );
+  await act(async () => fireEvent.click(screen.getByText(/Save Changes/i)));
   expect(terrasoApi.request).toHaveBeenCalledTimes(2);
   const saveCall = terrasoApi.request.mock.calls[1];
   expect(saveCall[1]).toStrictEqual({
@@ -234,9 +230,7 @@ test('LandscapeForm: Save form error', async () => {
   });
   fireEvent.change(inputs.location, { target: { value: 'New location' } });
 
-  await act(async () =>
-    fireEvent.click(screen.getByText(/Submit Landscape Info/i))
-  );
+  await act(async () => fireEvent.click(screen.getByText(/Save Changes/i)));
 
   // Test error display
   expect(screen.getByText(/Save Error/i)).toBeInTheDocument();
@@ -287,9 +281,7 @@ test('LandscapeForm: Save form (add)', async () => {
   });
   fireEvent.change(inputs.location, { target: { value: 'New location' } });
 
-  await act(async () =>
-    fireEvent.click(screen.getByText(/Submit Landscape Info/i))
-  );
+  await act(async () => fireEvent.click(screen.getByText(/Create Landscape/i)));
   expect(terrasoApi.request).toHaveBeenCalledTimes(1);
   const saveCall = terrasoApi.request.mock.calls[0];
   expect(saveCall[1]).toStrictEqual({
