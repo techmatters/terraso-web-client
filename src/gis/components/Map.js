8ae7c0cd686ae57e51e6c26ff0dbdf22e1b1c1bc
Paul Schreiber <paul@techmatters.org>
2022-04-20 16:34:55 -0400

    feature: add search to map component (#310)
    * feat: add search bar to map

--- src/gis/components/Map.js
+++ src/gis/components/Map.js
@@ -1,9 +1,78 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import L from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import { GeoJSON, MapContainer, TileLayer, useMap } from 'react-leaflet';
 import { v4 as uuidv4 } from 'uuid';
 
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/dist/geosearch.css';
 import 'gis/components/Map.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const LeafletSearch = ({ onPinLocationChange }) => {
+  const map = useMap();
+  const [pinLocation, setPinLocation] = useState();
+  const [zoomLevel, setZoomLevel] = useState();
+
+  useEffect(() => {
+    if (pinLocation && zoomLevel) {
+      onPinLocationChange({
+        pinLocation,
+        zoomLevel,
+      });
+    }
+  }, [zoomLevel, pinLocation, onPinLocationChange]);
+
+  useEffect(() => {
+    const provider = new OpenStreetMapProvider();
+
+    const searchControl = new GeoSearchControl({
+      provider,
+      marker: {
+        draggable: true,
+        mapMarkerIcon: L.Icon.Default,
+      },
+    });
+
+    map.addControl(searchControl);
+
+    const getPinData = event => {
+      const zoom = event?.sourceTarget?.getZoom();
+      if (zoom) {
+        setZoomLevel(zoom);
+      }
+
+      if (event?.location?.lat) {
+        setPinLocation({
+          lat: event.location.lat,
+          lng: event.location.lng,
+        });
+      }
+      if (event?.location?.x) {
+        setPinLocation({
+          lat: event.location.y,
+          lng: event.location.x,
+        });
+      }
+    };
+
+    map.on('geosearch/showlocation', getPinData);
+    map.on('geosearch/marker/dragend', getPinData);
+    map.on('zoomend', getPinData);
+
+    return () => map.removeControl(searchControl);
+  }, [map]);
+
+  return null;
+};
 
 const MapPolygon = props => {
   const { bounds, geojson } = props;
@@ -32,6 +101,11 @@ const Map = props => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MapPolygon {...props} />
+
+      {props.enableSearch && (
+        <LeafletSearch onPinLocationChange={props.onPinLocationChange} />
+      )}
+
       {props.children}
     </MapContainer>
   );
--- src/landscape/components/LandscapeMap.js
+++ src/landscape/components/LandscapeMap.js
@@ -10,7 +10,12 @@ import {
   isValidGeoJson,
 } from 'landscape/landscapeUtils';
 
-const LandscapeMap = ({ landscape, label }) => {
+const LandscapeMap = ({
+  landscape,
+  label,
+  onPinLocationChange,
+  enableSearch,
+}) => {
   const bounds = getLandscapeBoundingBox(landscape);
   const areaPolygon = _.get('areaPolygon', landscape);
   const geojson = isValidGeoJson(areaPolygon) ? areaPolygon : null;
@@ -19,6 +24,8 @@ const LandscapeMap = ({ landscape, label }) => {
       <Map
         bounds={bounds}
         geojson={geojson}
+        onPinLocationChange={onPinLocationChange}
+        enableSearch={enableSearch}
         style={{
           width: '100%',
           height: '400px',
