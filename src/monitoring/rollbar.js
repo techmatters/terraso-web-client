6e066310da98b64e58095102473eaa809990341a
Paul Schreiber <paul@techmatters.org>
2022-04-27 10:50:34 -0400

    fix: disable rollbar when token is not present (#320)

--- src/monitoring/rollbar.js
+++ src/monitoring/rollbar.js
@@ -5,6 +5,7 @@ import { TERRASO_ENV, ROLLBAR_TOKEN } from 'config';
 const rollbarConfig = {
   accessToken: ROLLBAR_TOKEN,
   environment: TERRASO_ENV,
+  enabled: !!ROLLBAR_TOKEN,
 };
 
 export const logLevel = 'warn';
