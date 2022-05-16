3d1d8199714389d3b60d5f0298646493ab5ffb68
Paul Schreiber <paul@techmatters.org>
2022-05-16 18:13:38 -0400

    fix: restrict access to SharedFilesCard

--- src/shared-files/SharedFilesCard.js
+++ src/shared-files/SharedFilesCard.js
@@ -11,6 +11,8 @@ import {
   Typography,
 } from '@mui/material';
 
+import Restricted from 'permissions/components/Restricted';
+
 import FileCard from './FileCard';
 
 import theme from 'theme';
@@ -24,28 +26,30 @@ const SharedFilesCard = ({ group }) => {
   }
 
   return (
-    <Card variant="outlined">
-      <CardHeader
-        disableTypography
-        title={
-          <Typography variant="h2" id="group-view-card-title">
-            {t('shared_files.title')}
+    <Restricted permission="group.viewFiles" resource={group}>
+      <Card variant="outlined">
+        <CardHeader
+          disableTypography
+          title={
+            <Typography variant="h2" id="group-view-card-title">
+              {t('shared_files.title')}
+            </Typography>
+          }
+        />
+        <CardContent>
+          {sharedFiles.map((item, index) => (
+            <FileCard key={index} file={item} group={group} />
+          ))}
+          <Typography
+            variant="body1"
+            sx={{ marginTop: theme.spacing(2), marginBottom: theme.spacing(2) }}
+          >
+            {t('shared_files.description')}
           </Typography>
-        }
-      />
-      <CardContent>
-        {sharedFiles.map((item, index) => (
-          <FileCard key={index} file={item} />
-        ))}
-        <Typography
-          variant="body1"
-          sx={{ marginTop: theme.spacing(2), marginBottom: theme.spacing(2) }}
-        >
-          {t('shared_files.description')}
-        </Typography>
-        <Button variant="outlined">{t('shared_files.upload_button')}</Button>
-      </CardContent>
-    </Card>
+          <Button variant="outlined">{t('shared_files.upload_button')}</Button>
+        </CardContent>
+      </Card>
+    </Restricted>
   );
 };
 
