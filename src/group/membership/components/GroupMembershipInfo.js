f41d5072ca7b22d525dfd5f21f90510528397609
Jose Buitron <jose.buitron@gmail.com>
2022-06-29 13:48:05 -0500

    feat: Restrict membership - group profile (#434)

---
+++ src/group/membership/components/GroupMembershipInfo.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import { Trans, useTranslation } from 'react-i18next';
+
+import CloseIcon from '@mui/icons-material/CancelPresentation';
+import InfoIcon from '@mui/icons-material/InfoOutlined';
+import {
+  Dialog,
+  DialogContent,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+
+const GroupMembershipInfo = () => {
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <IconButton onClick={() => setOpen(true)} aria-label="delete">
+        <InfoIcon />
+      </IconButton>
+      <Dialog open={open}>
+        <DialogContent>
+          <IconButton
+            aria-label={t('group.membership_card_info_close')}
+            onClick={() => setOpen(false)}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: theme => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography variant="h2" sx={{ mb: 2 }}>
+            {t('group.membership_card_info_closed_title')}
+          </Typography>
+          <Stack spacing={1}>
+            <Trans i18nKey="group.membership_card_info_close_description">
+              <Typography variant="body1">Step 1</Typography>
+              <Typography variant="body1">Step 2</Typography>
+            </Trans>
+          </Stack>
+          <Typography variant="h2" sx={{ mt: 4, mb: 2 }}>
+            {t('group.membership_card_info_open_title')}
+          </Typography>
+          <Typography variant="body1">
+            {t('group.membership_card_info_open_description')}
+          </Typography>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default GroupMembershipInfo;
