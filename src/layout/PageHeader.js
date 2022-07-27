2c3cdcf9a4285f77a3f7d36ec93a21896b481d2f
Paul Schreiber <paul@techmatters.org>
2022-07-27 17:45:44 -0400

    fix: add id to main heading

--- src/layout/PageHeader.js
+++ src/layout/PageHeader.js
@@ -5,6 +5,7 @@ import { Typography } from '@mui/material';
 const PageHeader = ({ header, typographyProps = {} }) => (
   <Typography
     variant="h1"
+    id="main-heading"
     sx={theme => ({ marginBottom: theme.spacing(3) })}
     {...typographyProps}
   >
