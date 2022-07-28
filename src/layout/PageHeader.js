f3c47b727d67c85268ad08c991c28541a2b29630
Paul Schreiber <paul@techmatters.org>
2022-07-28 13:12:26 -0400

    fix: Ensures Terraso image is part of `<h1>` on login screen (#471)
    * fix: allow children to be passed to PageHeader component
    * fix: make image part of login screen header

--- src/layout/PageHeader.js
+++ src/layout/PageHeader.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Typography } from '@mui/material';
 
-const PageHeader = ({ header, typographyProps = {} }) => (
+const PageHeader = ({ header, children, typographyProps = {} }) => (
   <Typography
     variant="h1"
     sx={theme => ({ marginBottom: theme.spacing(3) })}
     {...typographyProps}
   >
     {header}
+    {children}
   </Typography>
 );
 
