d603ca7aec7f420fb9e1652cea14e64c6f2f25b6
Paul Schreiber <paul@techmatters.org>
2022-08-31 17:17:18 -0400

    fix: add name to GroupMemberJoin button
    add group context to GroupMemberJoin to get access to owner

--- src/group/membership/components/GroupMemberJoin.js
+++ src/group/membership/components/GroupMemberJoin.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import _ from 'lodash/fp';
 import { useTranslation } from 'react-i18next';
 
 import { LoadingButton } from '@mui/lab';
 
+import { useGroupContext } from 'group/groupContext';
+
 const GroupMemberJoin = props => {
   const { t } = useTranslation();
+
+  const { owner } = useGroupContext();
+
   return (
     <LoadingButton
       variant="outlined"
-      aria-label={t(props.ariaLabel)}
+      aria-label={t(props.ariaLabel, {
+        name: _.get('name', owner),
+      })}
       onClick={props.onJoin}
       loading={props.loading}
       sx={{ flexGrow: 1 }}
