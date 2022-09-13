75f31d30ea04b754dc88f3ed8aa1422f551b66a8
Jose Buitron <jose.buitron@gmail.com>
2022-09-13 10:21:24 -0500

    fix: Simplified expression

--- src/gis/gisUtils.js
+++ src/gis/gisUtils.js
@@ -2,5 +2,4 @@ export const isValidLatitude = lat => lat >= -90 && lat <= 90;
 export const isValidLongitude = lng => lng >= -180 && lng <= 180;
 
 // From: https://gis.stackexchange.com/a/303362
-export const normalizeLongitude = lng =>
-  (lng = (((lng % 360) + 540) % 360) - 180);
+export const normalizeLongitude = lng => (((lng % 360) + 540) % 360) - 180;
