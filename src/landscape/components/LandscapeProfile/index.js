7d8c25a69742488e8c9e132b25fbd19476b75f35
Jose Buitron <jose.buitron@gmail.com>
2022-10-26 15:39:33 -0500

    fix: Removed not used import

--- src/landscape/components/LandscapeProfile/index.js
+++ src/landscape/components/LandscapeProfile/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import _ from 'lodash/fp';
 import { usePermission } from 'permissions';
