e8205f636425f7aa093c6c661903e3677bd03ff4
Jose Buitron <jose.buitron@gmail.com>
2022-11-01 09:57:41 -0500

    fix: Remove save for now from key info step

--- src/landscape/components/LandscapeForm/Actions.js
+++ src/landscape/components/LandscapeForm/Actions.js
@@ -43,13 +43,15 @@ const Actions = props => {
           {t('landscape.form_back')}
         </Button>
         <Stack direction={{ xs: 'column', sm: 'row' }} spacing={1}>
-          <Button
-            variant="outlined"
-            onClick={onSaveWrapper}
-            sx={{ pl: 2, pr: 2 }}
-          >
-            {t('landscape.form_save_now')}
-          </Button>
+          {onSave && (
+            <Button
+              variant="outlined"
+              onClick={onSaveWrapper}
+              sx={{ pl: 2, pr: 2 }}
+            >
+              {t('landscape.form_save_now')}
+            </Button>
+          )}
           <Button variant="contained" onClick={onNext} sx={{ pl: 6, pr: 6 }}>
             {nextLabel || t('landscape.form_next')}
           </Button>
