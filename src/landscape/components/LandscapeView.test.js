c0e1ee7c58fe4032cb10bb402c8ca88ec218ddf4
Jose Buitron <jose.buitron@gmail.com>
2022-11-16 10:19:50 -0500

    Feat: Landscape download boundary (#604)

--- src/landscape/components/LandscapeView.test.js
+++ src/landscape/components/LandscapeView.test.js
@@ -8,6 +8,9 @@ import { useParams } from 'react-router-dom';
 import LandscapeView from 'landscape/components/LandscapeView';
 import * as terrasoApi from 'terrasoBackend/api';
 
+const GEOJSON =
+  '{"type": "FeatureCollection", "features": [{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-80.02098083496094, 0.8184536092473124], [-80.04364013671875, 0.8177670337355836], [-80.04844665527342, 0.8184536092473124], [-80.04981994628906, 0.8260059320976082], [-80.07247924804686, 0.802662342941431], [-80.09170532226562, 0.779318620539376], [-80.10063171386719, 0.7532284249372649], [-80.09857177734375, 0.7223319390984623], [-80.09307861328125, 0.7140928403610857], [-80.10337829589842, 0.6955548144696846], [-80.09788513183594, 0.6742703246919985], [-80.08827209472656, 0.6488661346824502], [-80.07797241210938, 0.6495527361122139], [-80.06561279296875, 0.6522991408974699], [-80.06235122680664, 0.6468063298344634], [-80.02098083496094, 0.8184536092473124]]]}, "properties": {}}]}';
+
 jest.mock('terrasoBackend/api');
 
 jest.mock('react-router-dom', () => ({
@@ -38,6 +41,7 @@ beforeEach(() => {
   useParams.mockReturnValue({
     slug: 'slug-1',
   });
+  global.URL.createObjectURL = jest.fn();
 });
 
 const baseViewTest = async () => {
@@ -122,6 +126,7 @@ const baseViewTest = async () => {
               description: 'Landscape Description',
               website: 'www.landscape.org',
               location: 'EC',
+              areaPolygon: GEOJSON,
               defaultGroup: {
                 edges: [
                   {
@@ -186,6 +191,14 @@ const baseViewTest = async () => {
   const entriesList = within(sharedDataRegion.getByRole('list'));
   const items = entriesList.getAllByRole('listitem');
   expect(items.length).toBe(6);
+
+  // Boundary
+  expect(
+    screen.getByRole('region', { name: 'Landscape map' })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('button', { name: 'Download boundary (GeoJSON)' })
+  ).toBeInTheDocument();
 };
 
 test('LandscapeView: Display error', async () => {
