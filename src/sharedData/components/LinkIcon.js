faf90faa5045f343896a2faf2631f90352a4a56c
Jose Buitron <jose.buitron@gmail.com>
2022-12-07 12:07:32 -0500

    fix: Fixed Link SVG

--- src/sharedData/components/LinkIcon.js
+++ src/sharedData/components/LinkIcon.js
@@ -7,11 +7,17 @@ const LinkIcon = props => (
     <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M11.544 8.456a4.368 4.368 0 00-6.176 0L2.28 11.543a4.368 4.368 0 006.177 6.177L10 16.176"
-        stroke="#999"
+        stroke="gray"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
       <path
         d="M8.457 11.544a4.368 4.368 0 006.175 0l3.09-3.088a4.367 4.367 0 00-6.178-6.177l-1.543 1.544"
-        stroke="#999"
+        stroke="gray"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   </SvgIcon>
