0e39fbfe7962b47563d12c3d388ad5578b1f1273
Paul Schreiber <paul@techmatters.org>
2022-12-16 17:14:54 -0500

    fix: remove extra space from landscape area helper text popup

--- src/landscape/components/LandscapeForm/ProfileStep.js
+++ src/landscape/components/LandscapeForm/ProfileStep.js
@@ -182,7 +182,7 @@ const AreaTypeImage = props => {
 
 const AreaTypesHelperText = () => {
   return (
-    <Box sx={{ p: 2 }}>
+    <Box sx={{ p: 2, pt: 0 }}>
       <Trans i18nKey="landscape.form_profile_area_types_helper_text" />
       <Grid container spacing={2} justifyContent="center">
         <AreaTypeImage areaType="peri-urban" />
