52b0edafe5d4903e44fbd18d9bbe9db4bf7bab36
Jose Buitron <jose.buitron@gmail.com>
2023-01-18 12:29:48 -0500

    feat: (WIP) Mapbox storytelling tool

---
+++ src/user/components/UserStoryMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import StoryMap from 'storyMap/components/StoryMap';
+
+import config from './config';
+
+const UserStoryMap = () => {
+  return <StoryMap config={config} />;
+};
+
+export default UserStoryMap;
