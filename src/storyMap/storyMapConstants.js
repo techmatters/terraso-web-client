2f05a19c0fa59066cf329435fd98bdb7402cb5c0
Jose Buitron <jose.buitron@gmail.com>
2023-01-25 11:16:24 -0500

    feat: (WIP) Edit title and description

---
+++ src/storyMap/storyMapConstants.js
@@ -0,0 +1,16 @@
+export const LAYER_TYPES = {
+  fill: ['fill-opacity'],
+  line: ['line-opacity'],
+  circle: ['circle-opacity', 'circle-stroke-opacity'],
+  symbol: ['icon-opacity', 'text-opacity'],
+  raster: ['raster-opacity'],
+  'fill-extrusion': ['fill-extrusion-opacity'],
+  heatmap: ['heatmap-opacity'],
+};
+
+export const ALIGNMENTS = {
+  left: 'lefty',
+  center: 'centered',
+  right: 'righty',
+  full: 'fully',
+};
