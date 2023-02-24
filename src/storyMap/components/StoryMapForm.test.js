d374ee0d8994f7535f8149c1401f5faa0fe816d4
Jose Buitron <jose.buitron@gmail.com>
2023-02-24 12:24:22 -0500

    chore: Tests update

--- src/storyMap/components/StoryMapForm.test.js
+++ src/storyMap/components/StoryMapForm.test.js
@@ -21,7 +21,7 @@ import React from 'react';
 import mapboxgl from 'gis/mapbox';
 
 import StoryMapForm from './StoryMapForm';
-import { ConfigContextProvider } from './StoryMapForm/configContext';
+import { StoryMapConfigContextProvider } from './StoryMapForm/storyMapConfigContext';
 
 // Mock mapboxgl
 jest.mock('gis/mapbox', () => ({}));
@@ -34,10 +34,13 @@ beforeEach(() => {
     off: jest.fn(),
     getCanvas: jest.fn(),
   };
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
 });
 
 const BASE_CONFIG = {
   title: 'Story Map Title',
+  subtitle: 'Story Map Subtitle',
+  byline: 'by User',
   chapters: [
     {
       id: 'chapter-1',
@@ -58,10 +61,15 @@ const setup = async config => {
   const onSaveDraft = jest.fn();
 
   await render(
-    <ConfigContextProvider baseConfig={config}>
+    <StoryMapConfigContextProvider baseConfig={config}>
       <StoryMapForm onPublish={onPublish} onSaveDraft={onSaveDraft} />
-    </ConfigContextProvider>
+    </StoryMapConfigContextProvider>
   );
+
+  return {
+    onPublish,
+    onSaveDraft,
+  };
 };
 
 const testChapter = ({ title, description, image }) => {
@@ -71,10 +79,15 @@ const testChapter = ({ title, description, image }) => {
   expect(
     within(chapterSection).getByRole('heading', { name: title })
   ).toBeInTheDocument();
-  const descriptionTextbox = within(chapterSection).getByRole('textbox', {
-    name: 'Chapter description',
-  });
-  expect(within(descriptionTextbox).getByText(description)).toBeInTheDocument();
+
+  if (description) {
+    const descriptionTextbox = within(chapterSection).getByRole('textbox', {
+      name: 'Chapter description',
+    });
+    expect(
+      within(descriptionTextbox).getByText(description)
+    ).toBeInTheDocument();
+  }
 
   if (image) {
     const imageElement = within(chapterSection).getByRole('img', {
@@ -84,9 +97,65 @@ const testChapter = ({ title, description, image }) => {
   }
 };
 
+const changeChaper = async (title, newTitle, newDescription, newFile) => {
+  const chapterSection = screen.getByRole('region', {
+    name: `Chapter: ${title}`,
+  });
+  const titleTextbox = within(chapterSection).getByRole('textbox', {
+    name: 'Chapter title',
+  });
+  await act(async () =>
+    fireEvent.change(titleTextbox, { target: { value: newTitle } })
+  );
+
+  // TODO test rich text editor
+  // const descriptionTextbox = within(chapterSection).getByRole('textbox', {
+  //   name: 'Chapter description',
+  // });
+
+  if (newFile) {
+    const mediaButton = within(chapterSection).getByRole('button', {
+      name: 'Add media',
+    });
+    await act(async () => fireEvent.click(mediaButton));
+
+    const mediaDialog = screen.getByRole('dialog', {
+      name: 'Add media',
+    });
+    const dropZone = within(mediaDialog).getByRole('button', {
+      name: 'Upload a photo or audio file Select File Accepted file formats: *.aac, *.gif, *.jpeg, *.jpg, *.mp3, *.ogg, *.png, *.wav Maximum file size: 10 MB',
+    });
+    const data = {
+      dataTransfer: {
+        files: [newFile],
+        items: [
+          {
+            kind: 'file',
+            type: newFile.type,
+            getAsFile: () => newFile,
+          },
+        ],
+        types: ['Files'],
+      },
+    };
+    await act(async () => fireEvent.drop(dropZone, data));
+
+    await waitFor(() =>
+      expect(screen.getByRole('button', { name: 'Add' })).not.toHaveAttribute(
+        'disabled'
+      )
+    );
+
+    await act(async () =>
+      fireEvent.click(screen.getByRole('button', { name: 'Add' }))
+    );
+  }
+};
+
 test('StoryMapForm: Renders title and chapters correctly', async () => {
   await setup(BASE_CONFIG);
 
+  // Editor header
   const header = screen.getByRole('region', {
     name: '[TODO] Story editor Header',
   });
@@ -94,6 +163,117 @@ test('StoryMapForm: Renders title and chapters correctly', async () => {
     within(header).getByRole('heading', { name: 'Story Map Title' })
   ).toBeInTheDocument();
 
-  testChapter({ title: 'Chapter 1', description: 'Chapter 1 description', image: 'https://test.com/image.png' });
+  // Title section
+  const titleSection = screen.getByRole('region', {
+    name: 'Title for: Story Map Title',
+  });
+  expect(
+    within(titleSection).getByRole('heading', { name: 'Story Map Title' })
+  ).toBeInTheDocument();
+  expect(
+    within(titleSection).getByRole('heading', { name: 'Story Map Subtitle' })
+  ).toBeInTheDocument();
+  expect(within(titleSection).getByText('by User')).toBeInTheDocument();
+  // Outline
+  expect(
+    within(titleSection).getByRole('link', { name: 'Chapter 1' })
+  ).toBeInTheDocument();
+  expect(
+    within(titleSection).getByRole('link', { name: 'Chapter 2' })
+  ).toBeInTheDocument();
+
+  testChapter({
+    title: 'Chapter 1',
+    description: 'Chapter 1 description',
+    image: 'https://test.com/image.png',
+  });
   testChapter({ title: 'Chapter 2', description: 'Chapter 2 description' });
+});
+
+test('StoryMapForm: Adds new chapter', async () => {
+  const { onSaveDraft } = await setup(BASE_CONFIG);
+
+  // Add new chapter
+  const addChapterButton = screen.getByRole('button', {
+    name: 'Add new chapter',
+  });
+  await act(async () => fireEvent.click(addChapterButton));
+
+  // New chapter should be added
+  const newChapter = screen.getByRole('region', {
+    name: 'Chapter: (No title)',
+  });
+  expect(newChapter).toBeInTheDocument();
+
+  // Change title and description
+  await changeChaper(
+    '(No title)',
+    'New chapter',
+    'New chapter description',
+    new File(['content2'], `test.jpg`, {
+      type: `image/jpeg`,
+    })
+  );
+
+  // New chapter should be added to the outline
+  const titleSection = screen.getByRole('region', {
+    name: 'Title for: Story Map Title',
+  });
+  expect(
+    within(titleSection).getByRole('link', { name: 'New chapter' })
+  ).toBeInTheDocument();
+
+  // Save 
+  await act(async () =>
+    fireEvent.click(screen.getByRole('button', { name: 'Save draft' }))
+  );
+  expect(onSaveDraft).toHaveBeenCalledTimes(1);
+  const saveCall = onSaveDraft.mock.calls[0];
+  console.log(JSON.stringify(saveCall[0], null, 2));
+  console.log(JSON.stringify(saveCall[1], null, 2));
+  expect(saveCall[0]).toEqual(
+    expect.objectContaining({
+      title: 'Story Map Title',
+      subtitle: 'Story Map Subtitle',
+      byline: 'by User',
+    }),
+  );
+  expect(saveCall[0].chapters[0]).toEqual(
+    expect.objectContaining({
+        id: 'chapter-1',
+        title: 'Chapter 1',
+        description: 'Chapter 1 description',
+        media: {
+          type: 'image/png',
+          signedUrl: 'https://test.com/image.png',
+        },
+    }),
+  );
+  expect(saveCall[0].chapters[1]).toEqual(
+    expect.objectContaining({
+        id: 'chapter-2',
+        title: 'Chapter 2',
+        description: 'Chapter 2 description',
+    }),
+  );
+  expect(saveCall[0].chapters[2]).toEqual(
+    expect.objectContaining({
+        alignment: 'left',
+        title: 'New chapter',
+        description: '',
+        mapAnimation: 'flyTo',
+        rotateAnimation: false,
+        onChapterEnter: [],
+        onChapterExit: [],
+    }),
+  );
+  expect(saveCall[0].chapters[2].media).toEqual(
+    expect.objectContaining({
+          filename: 'test.jpg',
+          type: 'image/jpeg',
+    }),
+  );
+
+  expect(saveCall[0].chapters[2].media.contentId).toEqual(Object.keys(saveCall[1])[0]);
+
 });
