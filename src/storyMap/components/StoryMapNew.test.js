ef7f977ab648eaf386d5be3af0993d60dce001d4
Jose Buitron <jose.buitron@gmail.com>
2023-02-27 16:30:51 -0500

    chore: New and Update tests

---
+++ src/storyMap/components/StoryMapNew.test.js
@@ -0,0 +1,45 @@
+
+/*
+ * Copyright © 2021-2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import { act, fireEvent, render, screen } from 'tests/utils';
+import * as terrasoApi from 'terrasoBackend/api';
+
+import StoryMapNew from './StoryMapNew';
+
+jest.mock('terrasoBackend/api');
+
+jest.mock('./StoryMap', () => props => (
+  <div>Test</div>
+));
+
+test('StoryMapNew: Renders editor', async () => {
+  await render(<StoryMapNew />);
+
+  expect(screen.getByRole('region', { name: '[TODO] Story editor Header' })).toBeInTheDocument()
+  expect(screen.getByRole('navigation', { name: '[TODO] Chapters sidebar' })).toBeInTheDocument()
+});
+
+test('StoryMapNew: Save', async () => {
+  await render(<StoryMapNew />);
+
+  const saveButton = screen.getByRole('button', { name: 'Save draft' });
+  await act(async () =>
+  fireEvent.click(saveButton)
+  );
+
+  expect(terrasoApi.request).toHaveBeenCalledTimes(1);
+});
