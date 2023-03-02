c04ec2fd12a282cec9374322e71ee8255267aa69
Jose Buitron <jose.buitron@gmail.com>
2023-03-02 18:18:46 -0500

    fix: RichTextEditor tests

--- src/common/components/RichTextEditor/index.js
+++ src/common/components/RichTextEditor/index.js
@@ -25,7 +25,6 @@ import {
   createEditor,
 } from 'slate';
 import { withHistory } from 'slate-history';
-import { jsx } from 'slate-hyperscript';
 import { Editable, Slate, useSelected, useSlate, withReact } from 'slate-react';
 
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
@@ -50,6 +49,7 @@ import { withProps } from 'react-hoc';
 
 import ExternalLink from '../ExternalLink';
 import Toolbar from './Toolbar';
+import { deserialize } from './utils';
 
 import theme from 'theme';
 
@@ -96,60 +96,6 @@ const FORMAT_WRAPPERS = {
   italic: 'em',
 };
 
-const ELEMENT_TAGS = {
-  A: el => ({ type: 'link', url: el.getAttribute('href') }),
-  H1: () => ({ type: 'heading-one' }),
-  H2: () => ({ type: 'heading-two' }),
-  H3: () => ({ type: 'heading-three' }),
-  H4: () => ({ type: 'heading-four' }),
-  H5: () => ({ type: 'heading-five' }),
-  H6: () => ({ type: 'heading-six' }),
-  LI: () => ({ type: 'list-item' }),
-  OL: () => ({ type: 'numbered-list' }),
-  P: () => ({ type: 'paragraph' }),
-  UL: () => ({ type: 'bulleted-list' }),
-};
-
-const TEXT_TAGS = {
-  I: () => ({ italic: true }),
-  STRONG: () => ({ bold: true }),
-};
-
-export const deserialize = el => {
-  if (el.nodeType === Node.TEXT_NODE) {
-    return el.textContent;
-  } else if (el.nodeType !== Node.ELEMENT_NODE) {
-    return null;
-  } else if (el.nodeName === 'BR') {
-    return '\n';
-  }
-
-  const { nodeName } = el;
-  let parent = el;
-
-  let children = Array.from(parent.childNodes).map(deserialize).flat();
-
-  if (children.length === 0) {
-    children = [{ text: '' }];
-  }
-
-  if (el.nodeName === 'BODY') {
-    return jsx('fragment', {}, children);
-  }
-
-  if (ELEMENT_TAGS[nodeName]) {
-    const attrs = ELEMENT_TAGS[nodeName](el);
-    return jsx('element', attrs, children);
-  }
-
-  if (TEXT_TAGS[nodeName]) {
-    const attrs = TEXT_TAGS[nodeName](el);
-    return children.map(child => jsx('text', attrs, child));
-  }
-
-  return children;
-};
-
 const withInlines = editor => {
   const { insertData, insertText, isInline } = editor;
 
@@ -168,6 +114,7 @@ const withInlines = editor => {
     const html = data.getData('text/html');
 
     if (html) {
+      console.log({ html });
       const parsed = new DOMParser().parseFromString(html, 'text/html');
       const fragment = deserialize(parsed.body);
       Transforms.insertFragment(editor, fragment);
