decc0b36702ba6fe4c50e9c1edae60471ddbf529
Paul Schreiber <paul@techmatters.org>
2023-04-14 15:15:42 -0400

    fix: remove unused helmetContext variable

--- src/layout/AppWrappers.js
+++ src/layout/AppWrappers.js
@@ -17,6 +17,8 @@
 import React from 'react';
 
 import { PermissionsProvider } from 'permissions';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -24,6 +26,8 @@ import { ThemeProvider } from '@mui/material';
 
 import ErrorMonitoringProvider from 'monitoring/error';
 import NotificationsWrapper from 'notifications/NotificationsWrapper';
+
+import { REACT_APP_BASE_URL } from 'config';
 
 // Localization
 import 'localization/i18n';
@@ -40,30 +44,45 @@ import RefreshProgressProvider from './RefreshProgressProvider';
 
 // Wrappers
 // Router, Theme, Global State, Permissions, Notifications, Breadcrumbs
-const AppWrappers = ({ children, theme, store, permissionsRules }) => (
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <ErrorMonitoringProvider>
-          <Provider store={store}>
-            <RefreshProgressProvider>
-              <PermissionsProvider rules={permissionsRules}>
-                <NotificationsWrapper>
-                  <BreadcrumbsContextProvider>
-                    <SocialShareContextProvider>
-                      <ContainerContextProvider>
-                        {children}
-                      </ContainerContextProvider>
-                    </SocialShareContextProvider>
-                  </BreadcrumbsContextProvider>
-                </NotificationsWrapper>
-              </PermissionsProvider>
-            </RefreshProgressProvider>
-          </Provider>
-        </ErrorMonitoringProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const AppWrappers = ({ children, theme, store, permissionsRules }) => {
+  const { t } = useTranslation();
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <ErrorMonitoringProvider>
+            <Provider store={store}>
+              <RefreshProgressProvider>
+                <PermissionsProvider rules={permissionsRules}>
+                  <NotificationsWrapper>
+                    <BreadcrumbsContextProvider>
+                      <SocialShareContextProvider>
+                        <ContainerContextProvider>
+                          {children}
+                        </ContainerContextProvider>
+                      </SocialShareContextProvider>
+                    </BreadcrumbsContextProvider>
+                  </NotificationsWrapper>
+                </PermissionsProvider>
+              </RefreshProgressProvider>
+            </Provider>
+          </ErrorMonitoringProvider>
+        </ThemeProvider>
+        <HelmetProvider>
+          <Helmet>
+            <meta name="description" content={t('site.description')} />
+            <meta property="og:title" content={document.title} />
+            <meta property="og:description" content={t('site.description')} />
+            <meta
+              property="og:image"
+              content={`${REACT_APP_BASE_URL}/favicon.png`}
+            />
+          </Helmet>
+        </HelmetProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
 
 export default AppWrappers;
