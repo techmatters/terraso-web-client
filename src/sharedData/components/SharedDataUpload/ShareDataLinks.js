bbf366ba906a7aef08641cbdeac7ac2464540d67
jodiezhuu <jodiezhu12@gmail.com>
2023-05-13 20:25:17 -0400

    fix: API, web client URL validation inconsistent

--- src/sharedData/components/SharedDataUpload/ShareDataLinks.js
+++ src/sharedData/components/SharedDataUpload/ShareDataLinks.js
@@ -54,7 +54,14 @@ import { groupDataEntryUploadsByStatus } from './utils';
 
 export const VALIDATION_SCHEMA = yup
   .object({
-    url: yup.string().trim().transform(transformURL).urlCustom().required(),
+    url: yup
+      .string()
+      .trim()
+      .ensure()
+      .transform(transformURL)
+      .url()
+      .urlCustom()
+      .required(),
     name: yup.string().trim().required(),
     description: yup.string().max(MAX_DESCRIPTION_CHARACTERS).trim(),
   })
