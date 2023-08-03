ea58be69ee54f323f8965bd569517abd03860f88
jodiezhuu <jodiezhu12@gmail.com>
2023-08-03 17:05:13 -0400

    feat: add unit tests for user story maps

--- src/storyMap/components/StoryMapsToolHome.test.js
+++ src/storyMap/components/StoryMapsToolHome.test.js
@@ -21,35 +21,29 @@ import StoryMapsToolsHome from './StoryMapsToolHome';
 
 jest.mock('terraso-client-shared/terrasoApi/api');
 
-test('StoryMapsToolHome: renders correctly', async () => {
+test('StoryMapsToolHome: samples renders correctly', async () => {
   terrasoApi.requestGraphQL.mockReturnValue(
     Promise.resolve({
-      storyMaps: {
+      samples: {
         edges: [
           {
             node: {
               id: 'id-1',
               slug: 'id-1',
+              storyMapId: '46h36we',
               title: 'Story 1',
               isPublished: false,
-              publishedAt: '2023-01-31T22:25:42.916303+00:00',
-              createdBy: {
-                firstName: 'First',
-                lastName: 'Last',
-              },
+              updatedAt: '2023-01-31T22:25:42.916303+00:00',
             },
           },
           {
             node: {
               id: 'id-2',
               slug: 'id-2',
+              storyMapId: 'lftawa9',
               title: 'Story 2',
               isPublished: true,
-              publishedAt: '2023-01-31T22:25:42.916303+00:00',
-              createdBy: {
-                firstName: 'First',
-                lastName: 'Last',
-              },
+              updatedAt: '2023-01-31T22:25:42.916303+00:00',
             },
           },
         ],
@@ -73,9 +67,8 @@ test('StoryMapsToolHome: renders correctly', async () => {
   expect(
     screen.getByRole('link', { name: 'Create Story Map' })
   ).toBeInTheDocument();
-
   expect(
-    screen.getByRole('heading', {
+    screen.getByRole('region', {
       name: 'Sample story maps: View what others created',
     })
   ).toBeInTheDocument();
@@ -88,6 +81,65 @@ test('StoryMapsToolHome: renders correctly', async () => {
   expect(
     within(items[0]).getByRole('link', { name: 'Story 1' })
   ).toBeInTheDocument();
-  expect(within(items[0]).getByText(/by First Last/i)).toBeInTheDocument();
-  expect(within(items[0]).getByText(/Published on/)).toBeInTheDocument();
+  expect(
+    within(items[0]).getByText(/By {{user.firstName}} {{user.lastName}}/i)
+  ).toBeInTheDocument();
+});
+
+test('StoryMapsToolHome: user story maps render correctly', async () => {
+  terrasoApi.requestGraphQL.mockReturnValue(
+    Promise.resolve({
+      userStoryMaps: {
+        edges: [
+          {
+            node: {
+              id: 'id-1',
+              slug: 'id-1',
+              storyMapId: '46h36we',
+              title: 'Story 1',
+              isPublished: false,
+              updatedAt: '2023-01-31T22:25:42.916303+00:00',
+            },
+          },
+          {
+            node: {
+              id: 'id-2',
+              slug: 'id-2',
+              storyMapId: 'lftawa9',
+              title: 'Story 2',
+              isPublished: true,
+              updatedAt: '2023-01-31T22:25:42.916303+00:00',
+            },
+          },
+        ],
+      },
+    })
+  );
+
+  await render(<StoryMapsToolsHome />, {
+    account: {
+      currentUser: {
+        data: {
+          email: 'account@email.com',
+        },
+      },
+    },
+  });
+
+  expect(
+    screen.getByRole('heading', { name: 'Terraso Story Maps' })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('link', { name: 'Create Story Map' })
+  ).toBeInTheDocument();
+  const list = screen.getByRole('region', {
+    name: 'Story Maps',
+  });
+  const items = within(list).getAllByRole('listitem');
+  expect(items.length).toBe(2);
+
+  const link1 = within(items[0]).getByRole('link', { name: 'Story 1' });
+  expect(link1).toHaveAttribute('href', '/tools/story-maps/46h36we/id-1/edit');
+  const link2 = within(items[1]).getByRole('link', { name: 'Story 2' });
+  expect(link2).toHaveAttribute('href', '/tools/story-maps/lftawa9/id-2');
 });
