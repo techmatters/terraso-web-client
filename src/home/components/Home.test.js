eaf4b6b7734ee042104e1fa6de35cd4e931b68b8
jodiezhuu <jodiezhu12@gmail.com>
2023-08-08 13:05:43 -0400

    chore: update story map order in homepage tests

--- src/home/components/Home.test.js
+++ src/home/components/Home.test.js
@@ -224,10 +224,10 @@ test('Home: Display Story Maps', async () => {
   const items = list.getAllByRole('listitem');
   expect(items.length).toBe(2);
 
-  const link1 = within(items[0]).getByRole('link', { name: 'Story 1' });
-  expect(link1).toHaveAttribute('href', '/tools/story-maps/46h36we/id-1/edit');
-  const link2 = within(items[1]).getByRole('link', { name: 'Story 2' });
-  expect(link2).toHaveAttribute('href', '/tools/story-maps/lftawa9/id-2');
+  const link1 = within(items[0]).getByRole('link', { name: 'Story 2' });
+  expect(link1).toHaveAttribute('href', '/tools/story-maps/lftawa9/id-2');
+  const link2 = within(items[1]).getByRole('link', { name: 'Story 1' });
+  expect(link2).toHaveAttribute('href', '/tools/story-maps/46h36we/id-1/edit');
 });
 test('Home: Display defaults', async () => {
   fetchHomeData.mockReturnValue(
