208c02faec1a29bf52016a4590d59fc7ef6b7855
Jose Buitron <jose.buitron@gmail.com>
2023-09-08 11:03:20 -0500

    fix: Added more deatils for drag and drop issue

--- src/sharedData/visualization/components/VisualizationConfigForm/AnnotateStep.js
+++ src/sharedData/visualization/components/VisualizationConfigForm/AnnotateStep.js
@@ -82,7 +82,12 @@ const FORM_FIELDS = [
   },
 ];
 
-// Work around for react-beautiful-dnd issue. See: https://stackoverflow.com/a/75807063
+// Work around for react-beautiful-dnd issue. The issues is that the
+// Droppable component is not compatible with React.StrictMode.
+// StricMode is used in the development environment by default.
+// See:
+// - https://stackoverflow.com/a/75807063
+// - https://github.com/atlassian/react-beautiful-dnd/issues/2396
 const StrictModeDroppable = ({ children, ...props }) => {
   const [enabled, setEnabled] = useState(false);
   useEffect(() => {
