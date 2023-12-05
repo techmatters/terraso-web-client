465ee2a046edac39d7bd41eddcfce2d236b4f9ee
Paul Schreiber <paul@techmatters.org>
2023-12-05 15:50:05 -0500

    fix: add sentry config init

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 import React from 'react';
+import * as Sentry from '@sentry/react';
 
 import 'config';
 
@@ -30,6 +31,22 @@ import theme from 'theme';
 import 'index.css';
 
 import App from 'App';
+
+Sentry.init({
+  dsn: 'https://dac0c436847fc922e68de0855b5ff738@o4506299756642304.ingest.sentry.io/4506345074130944',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById('root')).render(
   <AppWrappers store={createStore()} theme={theme} permissionsRules={rules}>
