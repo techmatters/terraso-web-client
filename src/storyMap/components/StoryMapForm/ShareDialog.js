e9853cedce86ce78fce86e597d78b9887e361dfe
Jose Buitron <jose.buitron@gmail.com>
2023-12-20 10:04:14 -0500

    fix: Set user field for owner membership

--- src/storyMap/components/StoryMapForm/ShareDialog.js
+++ src/storyMap/components/StoryMapForm/ShareDialog.js
@@ -218,7 +218,7 @@ const ShareDialog = props => {
   const memberships = useMemo(
     () => [
       {
-        ...storyMap.createdBy,
+        user: storyMap.createdBy,
         userRole: MEMBERSHIP_ROLE_OWNER,
       },
       ...storyMap.memberships,
