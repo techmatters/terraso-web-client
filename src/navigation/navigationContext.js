67ffcfcd8d1dd110fccb42ed59cf032a6760102d
Ruxandra Machedon <ruxandra@techmatters.org>
2024-03-18 13:06:09 -0400

    fix: replace ad-hoc member with module variable

--- src/navigation/navigationContext.js
+++ src/navigation/navigationContext.js
@@ -20,6 +20,8 @@ import {
   useNavigate,
 } from 'react-router-dom';
 
+let originalPush;
+
 export const useNavigationBlocker = (when, message) => {
   const navigate = useNavigate();
   const { navigator } = useContext(NavigationContext);
@@ -31,8 +33,8 @@ export const useNavigationBlocker = (when, message) => {
    * so that we can be sure we're restoring the original when the blocker is
    * disabled or otherwised cleaned up.
    */
-  if (!navigator.originalPush) {
-    navigator.originalPush = navigator.push;
+  if (!originalPush) {
+    originalPush = navigator.push;
   }
 
   const proceed = useCallback(() => {
@@ -58,7 +60,7 @@ export const useNavigationBlocker = (when, message) => {
 
   const disable = useCallback(() => {
     window.removeEventListener('beforeunload', preventNavigation);
-    navigator.push = navigator.originalPush;
+    navigator.push = originalPush;
   }, [preventNavigation, navigator]);
 
   useEffect(() => {
@@ -71,7 +73,7 @@ export const useNavigationBlocker = (when, message) => {
     navigator.push = (...args) => {
       const options = args[2];
       if (options?.force) {
-        navigator.originalPush(...args);
+        originalPush(...args);
         return;
       }
 
