techmatters/terraso-web-client

commit 8d661ac42ea4bdf588ab5f7defafccc815d7208f

feat: Add file extension type column to shared data card for files (#1660)
* feat: Add the file type to shared files info as a column in a nested grid.

* feat: remove nested grid and just shrink the space used for the title in order to accomodate the file type column

* docs: remove comments that were just notes to self

* fix: move uppercasing text to the view layer. This also fixes failing tests.

* test: add expectation of a file name and its extension type

src/sharedData/components/SharedDataEntryBase.js
@@ -54,7 +54,8 @@ const SharedDataEntryBase = props => {
     EntryTypeIcon,
     DownloadComponent,
     ShareComponent,
-    info,
+    fileSize,
+    resourceType,
     deleteTooltip,
   } = props;
   const [isEditingName, setIsEditingName] = useState(false);
@@ -173,7 +174,7 @@ const SharedDataEntryBase = props => {
         <Grid
           item
           xs={isEditingName ? 12 : 8}
-          md={6}
+          md={5}
           order={{ xs: 2, md: 2 }}
           component={StackRow}
         >
@@ -203,26 +204,38 @@ const SharedDataEntryBase = props => {
             />
           </Restricted>
         </Grid>
-        <Grid item xs={1} order={{ xs: 5 }} display={{ md: 'none' }} />
+
+        <Grid item xs={1} order={{ xs: 4 }} display={{ md: 'none' }} />
         <Grid
           item
-          xs={11}
+          xs={2}
           md={1}
-          order={{ xs: 6, md: 4 }}
+          order={{ xs: 5, md: 4 }}
+          sx={{ wordWrap: 'break-word', textTransform: 'uppercase' }}
+        >
+          {resourceType}
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          md={1}
+          order={{ xs: 6, md: 5 }}
           sx={{ wordWrap: 'break-word' }}
         >
-          {info}
+          {fileSize}
         </Grid>
+        <Grid item xs={7} order={{ xs: 7 }} display={{ md: 'none' }} />
         <Grid item xs={1} order={{ xs: 7 }} display={{ md: 'none' }} />
-        <Grid item xs={11} md={3} order={{ xs: 8, md: 4 }}>
+        <Grid item xs={11} md={3} order={{ xs: 8, md: 6 }}>
           {formatDate(i18n.resolvedLanguage, dataEntry.createdAt)}, by{' '}
           {t('user.full_name', { user: dataEntry.createdBy })}
         </Grid>
+
         <Grid
           item
           xs={4}
           md={2}
-          order={{ xs: 3, md: 4 }}
+          order={{ xs: 3, md: 6 }}
           component={StackRow}
           justifyContent="flex-end"
           display={isEditingName ? 'none' : 'inherit'}

src/sharedData/components/SharedDataEntryFile.js
@@ -362,7 +362,8 @@ const SharedDataEntryFile = props => {
       EntryTypeIcon={SharedFileIcon}
       DownloadComponent={DownloadComponent}
       ShareComponent={ShareComponent}
-      info={filesize(sharedResource.dataEntry.size, { round: 0 })}
+      fileSize={filesize(sharedResource.dataEntry.size, { round: 0 })}
+      resourceType={sharedResource.dataEntry.resourceType}
     >
       <Visualizations file={sharedResource.dataEntry} />
     </SharedDataEntryBase>

src/landscape/components/LandscapeView.test.js
@@ -122,6 +122,7 @@ const baseViewTest = async (
             description: `Description ${index}`,
             size: 3456,
             entryType: 'FILE',
+            resourceType: 'txt',
             visualizations: { edges: [] },
           },
         },
@@ -233,6 +234,9 @@ test('LandscapeView: Display data', async () => {
   const entriesList = within(sharedDataRegion.getByRole('list'));
   const items = entriesList.getAllByRole('listitem');
   expect(items.length).toBe(6);
+  const firstEntry = within(items[0]);
+  expect(firstEntry.getByText('Data Entry 0')).toBeInTheDocument();
+  expect(firstEntry.getByText('txt')).toBeInTheDocument();
 
   // Boundary
   expect(

commit a5e14de18c87fd2da67cdc8b1405d82bc4221ad3

fix: Display the domain url for links that was previously missing (#1694)
Displaying the domain was tied to the info prop. The bug was caused when I removed/renamed info, such that it then only applied to files, and failed to pass the domain info through for links.
The info concept is now replaced by InfoComponent -- whose implementation will differ for files and links, as they will show different info with different layouts.

Fixes #1690 
(which was caused by #1660)

Commits:
* fix: Display domain url for links in shared files again

* refactor: create InfoComponent for Link and File, instead of conditional logic in Base

* refactor: Wrap InfoComponent in a Grid item and change the file and link implementations with respect to that parent Grid item

It's not quite a refactor because the spacing changes very slightly, but I'm just gonna call it a refactor.

* chore: Clean up comments

* test: Add test that the expected items are in the shared data entries for files and links

* refactor: Change Grid container to React fragment

src/sharedData/components/SharedDataEntryBase.js
@@ -54,8 +54,7 @@ const SharedDataEntryBase = props => {
     EntryTypeIcon,
     DownloadComponent,
     ShareComponent,
-    fileSize,
-    resourceType,
+    InfoComponent,
   } = props;
   const [isEditingName, setIsEditingName] = useState(false);
   const [isEditingDescription, setIsEditingDescription] = useState(false);
@@ -214,23 +213,16 @@ const SharedDataEntryBase = props => {
         <Grid item xs={1} order={{ xs: 4 }} display={{ md: 'none' }} />
         <Grid
           item
-          xs={2}
-          md={1}
+          xs={9}
+          md={2}
           order={{ xs: 5, md: 4 }}
-          sx={{ wordWrap: 'break-word', textTransform: 'uppercase' }}
+          component={StackRow}
+          justifyContent="space-between"
         >
-          {resourceType}
+          {InfoComponent && <InfoComponent sharedResource={sharedResource} />}
         </Grid>
-        <Grid
-          item
-          xs={2}
-          md={1}
-          order={{ xs: 6, md: 5 }}
-          sx={{ wordWrap: 'break-word' }}
-        >
-          {fileSize}
-        </Grid>
-        <Grid item xs={7} order={{ xs: 7 }} display={{ md: 'none' }} />
+        <Grid item xs={2} order={{ xs: 6 }} display={{ md: 'none' }} />
+
         <Grid item xs={1} order={{ xs: 7 }} display={{ md: 'none' }} />
         <Grid item xs={11} md={3} order={{ xs: 8, md: 6 }}>
           {t('sharedData.file_date_and_author', {

src/sharedData/components/SharedDataEntryFile.js
@@ -62,6 +62,27 @@ const StackRow = props => (
   <Stack direction="row" alignItems="center" spacing={1} {...props} />
 );
 
+const InfoComponent = ({ sharedResource }) => {
+  const fileSize = filesize(sharedResource.dataEntry.size, { round: 0 });
+  const fileType = sharedResource.dataEntry.resourceType;
+  return (
+    <>
+      <Grid
+        item
+        xs={3}
+        md={6}
+        sx={{ wordWrap: 'break-word', textTransform: 'uppercase' }}
+      >
+        {fileType}
+      </Grid>
+      <Grid item xs={8} md={6} sx={{ wordWrap: 'break-word' }}>
+        {fileSize}
+      </Grid>
+      <Grid item xs={1} display={{ md: 'none' }} />
+    </>
+  );
+};
+
 const Visualizations = props => {
   const { baseOwnerUrl } = useCollaborationContext();
   const { i18n, t } = useTranslation();
@@ -373,8 +394,7 @@ const SharedDataEntryFile = props => {
       )}
       DownloadComponent={DownloadComponent}
       ShareComponent={ShareComponent}
-      fileSize={filesize(sharedResource.dataEntry.size, { round: 0 })}
-      resourceType={sharedResource.dataEntry.resourceType}
+      InfoComponent={InfoComponent}
     >
       <Visualizations file={sharedResource.dataEntry} />
     </SharedDataEntryBase>

src/sharedData/components/SharedDataEntryLink.js
@@ -17,7 +17,7 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { IconButton } from '@mui/material';
+import { Grid, IconButton } from '@mui/material';
 
 import { useCollaborationContext } from 'collaboration/collaborationContext';
 import ExternalLink from 'common/components/ExternalLink';
@@ -60,9 +60,7 @@ const DownloadComponent = props => {
   );
 };
 
-const SharedDataEntryLink = props => {
-  const { t } = useTranslation();
-  const { sharedResource } = props;
+const InfoComponent = ({ sharedResource }) => {
   const dataEntry = useMemo(
     () => sharedResource.dataEntry,
     [sharedResource.dataEntry]
@@ -72,6 +70,17 @@ const SharedDataEntryLink = props => {
     const url = new URL(dataEntry.url);
     return url.hostname;
   }, [dataEntry.url]);
+
+  return (
+    <Grid item xs={12} md={12} sx={{ wordWrap: 'break-word' }}>
+      {domain}
+    </Grid>
+  );
+};
+
+const SharedDataEntryLink = props => {
+  const { t } = useTranslation();
+  const { sharedResource } = props;
 
   return (
     <SharedDataEntryBase
@@ -87,8 +96,8 @@ const SharedDataEntryLink = props => {
           }}
         />
       )}
+      InfoComponent={InfoComponent}
       DownloadComponent={DownloadComponent}
-      info={domain}
     />
   );
 };

src/landscape/components/LandscapeView.test.js
@@ -109,6 +109,7 @@ const baseViewTest = async (
       })),
   };
 
+  // Add files to the shared resources
   const sharedResources = {
     edges: Array(6)
       .fill(0)
@@ -129,6 +130,25 @@ const baseViewTest = async (
         },
       })),
   };
+  // Add a link to the shared resources
+  let index = 6;
+  sharedResources.edges.push({
+    node: {
+      id: `sr-${index}`,
+      source: {
+        id: `de-${index}`,
+        createdAt: '2023-05-20T16:25:21.536679+00:00',
+        name: `Data Entry ${index}`,
+        createdBy: { id: 'user-id', firstName: 'First', lastName: 'Last' },
+        description: `Description ${index}`,
+        size: null,
+        entryType: 'LINK',
+        resourceType: 'link',
+        url: `https://www.link-${index}.com`,
+        visualizations: { edges: [] },
+      },
+    },
+  });
 
   when(terrasoApi.requestGraphQL)
     .calledWith(
@@ -218,10 +238,22 @@ test('LandscapeView: Display data', async () => {
   ).toBeInTheDocument();
   const entriesList = within(sharedDataRegion.getByRole('list'));
   const items = entriesList.getAllByRole('listitem');
-  expect(items.length).toBe(6);
-  const firstEntry = within(items[0]);
-  expect(firstEntry.getByText('Data Entry 0')).toBeInTheDocument();
-  expect(firstEntry.getByText('txt')).toBeInTheDocument();
+  expect(items.length).toBe(7);
+  const fileEntry = within(items[0]);
+  expect(fileEntry.getByText('Data Entry 0')).toBeInTheDocument();
+  expect(fileEntry.getByText('txt')).toBeInTheDocument();
+  expect(fileEntry.getByText('3 kB')).toBeInTheDocument();
+  expect(
+    fileEntry.getByText('May 20, 2022, by First Last')
+  ).toBeInTheDocument();
+  expect(fileEntry.getByText('Description 0')).toBeInTheDocument();
+  const linkEntry = within(items[6]);
+  expect(linkEntry.getByText('Data Entry 6')).toBeInTheDocument();
+  expect(linkEntry.getByText('www.link-6.com')).toBeInTheDocument();
+  expect(
+    linkEntry.getByText('May 20, 2023, by First Last')
+  ).toBeInTheDocument();
+  expect(linkEntry.getByText('Description 6')).toBeInTheDocument();
 
   // Boundary
   expect(