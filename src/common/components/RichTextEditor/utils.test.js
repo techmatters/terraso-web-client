8150d886a6aabb36506264905d0ad725639cf54d
Paul Schreiber <paul@techmatters.org>
2024-08-14 16:04:12 -0400

    fix: use import instead of require for deserialize (#2275)

--- src/common/components/RichTextEditor/utils.test.js
+++ src/common/components/RichTextEditor/utils.test.js
@@ -1,4 +1,21 @@
-const { deserialize } = require('./utils');
+/*
+ * Copyright © 2023 Technology Matters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import { deserialize } from 'common/components/RichTextEditor/utils';
 
 test('RichTextEditorUtils: deserialize', () => {
   const html = `<meta charset='utf-8'>
