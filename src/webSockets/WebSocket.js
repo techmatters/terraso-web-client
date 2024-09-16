276c42505ec10062dbfc3cbf92296a1661743696
Jose Buitron <jose.buitron@gmail.com>
2024-09-16 15:44:56 -0500

    feat: (WIP) Websockets configuration

---
+++ src/webSockets/WebSocket.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+const WebSocketComponent = props => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://127.0.0.1:8000/ws/shared-data/');
+
+    socket.onopen = () => {
+      console.log('WebSocket connection opened');
+    };
+
+    socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+      setMessage(data.message);
+    };
+
+    socket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Message from WebSocket: {message}</p>
+    </div>
+  );
+};
+
+export default WebSocketComponent;
