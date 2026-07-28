761aa6adfa0f00472b75df97feade5216f56b210
Jose Buitron <jose.buitron@gmail.com>
2026-07-28 12:21:10 -0500

    feat(story-map): add tool home card background

--- src/storyMap/components/StoryMapsCard.jsx
+++ src/storyMap/components/StoryMapsCard.jsx
@@ -60,6 +60,10 @@ const HOME_HEADING_SX = {
   wordWrap: 'break-word',
 };
 
+const TOOL_HOME_CARD_SX = {
+  bgcolor: theme => theme.pageContentBand.background,
+};
+
 const HOME_STORY_MAPS_SKELETON_SX = {
   bgcolor: 'rgba(255, 255, 255, 0.24)',
 };
@@ -188,7 +192,7 @@ const StoryMapsCard = ({
       title={title}
       titleId="story-maps-list-title"
       action={action}
-      cardSx={isHomeFeatureCard ? HOME_CARD_SX : undefined}
+      cardSx={isHomeFeatureCard ? HOME_CARD_SX : TOOL_HOME_CARD_SX}
       headingSx={isHomeFeatureCard ? HOME_HEADING_SX : undefined}
       backgroundImage={
         isHomeFeatureCard ? '/files/card-background-primary-1.png' : undefined
